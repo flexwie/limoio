b52a5f2ba0328bacc0ebb3ad3f71553728bb4bd2
Felix Wieland <flexwie@googlemail.com>
2017-01-29 14:52:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 var app = express()
 
 app.get('/', function(req, res) {
-	res.send('nice es geht')
+	res.send('nice es geht ab')
 })
 
 app.listen(4000, function() {
